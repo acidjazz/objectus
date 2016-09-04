24d2216797c1faca33d40038488c185b15d86965
kevin olson <acidjazz@gmail.com>
2016-09-04 00:49:27 -0700

    quotes for codacy

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 
-var gulp = require('gulp');
-var coffee = require('gulp-coffee');
-var notify = require('gulp-notify');
+var gulp = require("gulp");
+var coffee = require("gulp-coffee");
+var notify = require("gulp-notify");
 
-gulp.task('coffee', function() {
-  gulp.src('src/*.coffee')
+gulp.task("coffee", function() {
+  gulp.src("src/*.coffee")
     .pipe(coffee({bare: true})
-      .on('error', notify.onError(function(error) {
-        return {title: "Coffee error", message: error.message + "\r\n" + error.filename + ':' + error.location.first_line, sound: 'Pop'};
+      .on("error", notify.onError(function(error) {
+        return {title: "Coffee error", message: error.message + "\r\n" + error.filename + ":" + error.location.first_line, sound: "Pop"};
       }))
     )
-    .pipe(gulp.dest('./src'))
+    .pipe(gulp.dest("./src"));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.coffee', ['coffee']);
+gulp.task("watch", function() {
+  gulp.watch("src/*.coffee", ["coffee"]);
 });
 
-gulp.task('default', ['coffee']);
+gulp.task("default", ["coffee"]);
